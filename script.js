16ddd535d5175016639866b9fe94169a7e61613f
EricMB <ericmorais198@gmail.com>
2023-03-21 19:03:02 -0300

    add do meu portfolio

---
+++ script.js
@@ -0,0 +1,60 @@
+function mobileMenu() {
+    let mobileMenu = document.querySelector('.mobile-menu');
+    if (mobileMenu.classList.contains('open')){
+        mobileMenu.classList.remove('open')
+        document.querySelector('#mobile-menu-button').src="img/mobile-menu-icon-closed.png"
+    } else {
+        mobileMenu.classList.add('open')
+        document.querySelector('#mobile-menu-button').src="img/mobile-menu-icon-opened.png"
+    }
+}
+
+window.sr = ScrollReveal({distance: '30px',
+easing: 'ease-out', reset:true});
+
+sr.reveal('.img-box-home',{
+    origin:'left',
+    duration: 800
+});
+
+sr.reveal('.text-box-home', {
+    origin: 'right',
+    duration: 800
+});
+
+sr.reveal('.text-box-about-me', {
+    origin: 'right',
+    duration:800
+})
+
+sr.reveal('.img-box-about-me',{
+    origin:'left',
+    duration: 500
+});
+
+sr.reveal('.title-box-skills',{
+    origin:'top',
+     duration: 500
+});
+
+sr.reveal('.logo-box', {
+    duration: 900
+});
+
+sr.reveal('.two',{
+    origin:'bottom'
+});
+
+sr.reveal('.one', {
+    origin: 'top'
+});
+
+sr.reveal('.title-box-contact', {
+    origin:'top',
+    duration:500
+});
+
+sr.reveal('.social-medias-box-contact', {
+    origin: 'bottom',
+    duration: 900
+})
